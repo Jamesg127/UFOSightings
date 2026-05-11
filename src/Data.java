49c0d397fcbba846459e735f1444ce6bf626a779
Theo Kozak <theokozak@gmail.com>
2026-05-11 10:52:20 -0600

    fixed james code

--- src/Data.java
+++ src/Data.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -5,7 +11,31 @@ public class Data {
     ArrayList<UFOReport> reports;
 
     void readData() {
-        //TODO use UFO Reader to read data
+        try {
+            UFOReader reader = new UFOReader();
+            var data = reader.readFromFile();
+            for(String line : data) {
+                String[] components = line.split(",");
+                reports.add(
+                        new UFOReport(
+                                LocalDateTime.parse(
+                                        components[0],
+                                        DateTimeFormatter.ofPattern("M/d/uu HH:MM")
+                                )
+                                        .toInstant(ZoneOffset.ofHours(0)),
+                                components[1],
+                                components[2],
+                                components[3],
+                                components[4],
+                                components[5],
+                                Double.parseDouble(components[6]),
+                                Double.parseDouble(components[7])
+                        )
+                );
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     void sortByDate() {
@@ -19,5 +49,8 @@ public class Data {
     }
     void sortByShape() {
         reports.sort(UFOReport.getShapeComparator());
+    }
+
+    ArrayList searchState(String state) {
     }
 }

35393d418993e88181e6e6b2ceb49c827d27ac98
jonathanbuck687 <jonathanb138@lpsk12.org>
2026-05-11 10:32:15 -0600

    mamma mia

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello and welcome to the UFO report user interface");
-
+        System.out.println("Hello and welcome to theUFO report user interface.");
+        System.out.println("What would you like to do?");
+        System.out.println("1) Sort the data\n2) ")
     }
 }
-
-

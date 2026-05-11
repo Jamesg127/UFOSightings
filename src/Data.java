8a853d3a688b2e15719ffea32d2d66621bac2cf2
Theo Kozak <theokozak@gmail.com>
2026-05-11 10:26:20 -0600

    sorting

--- src/Data.java
+++ src/Data.java
@@ -1,9 +1,23 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Data {
     ArrayList<UFOReport> reports;
 
     void readData() {
         //TODO use UFO Reader to read data
+    }
+
+    void sortByDate() {
+        Collections.sort(reports, UFOReport.getDateComparator());
+    }
+    void sortByCity() {
+        Collections.sort(reports, UFOReport.getCityComparator());
+    }
+    void sortByState() {
+        Collections.sort(reports, UFOReport.getStateComparator());
+    }
+    void sortByShape() {
+        Collections.sort(reports, UFOReport.getShapeComparator());
     }
 }
--- src/UFOReport.java
+++ src/UFOReport.java
@@ -1,5 +1,8 @@
+import java.time.Instant;
+import java.util.Comparator;
+
 public class UFOReport {
-    String dateTime;
+    Instant dateTime;
     String country;
     String city;
     String state;
@@ -7,4 +10,37 @@ public class UFOReport {
     String summary;
     double lat;
     double lng;
+
+    static Comparator<UFOReport> getDateComparator() {
+        return new Comparator<UFOReport>() {
+            @Override
+            public int compare(UFOReport o1, UFOReport o2) {
+                return o1.dateTime.compareTo(o2.dateTime);
+            }
+        };
+    }
+    static Comparator<UFOReport> getCityComparator() {
+        return new Comparator<UFOReport>() {
+            @Override
+            public int compare(UFOReport o1, UFOReport o2) {
+                return o1.city.compareTo(o2.city);
+            }
+        };
+    }
+    static Comparator<UFOReport> getStateComparator() {
+        return new Comparator<UFOReport>() {
+            @Override
+            public int compare(UFOReport o1, UFOReport o2) {
+                return o1.state.compareTo(o2.state);
+            }
+        };
+    }
+    static Comparator<UFOReport> getShapeComparator() {
+        return new Comparator<UFOReport>() {
+            @Override
+            public int compare(UFOReport o1, UFOReport o2) {
+                return o1.shape.compareTo(o2.shape);
+            }
+        };
+    }
 }

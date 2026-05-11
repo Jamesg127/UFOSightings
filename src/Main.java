a90f72cd1e427be7217052a6315cb580357bda0c
jonathanbuck687 <jonathanb138@lpsk12.org>
2026-05-11 10:45:59 -0600

    mamma mia

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Data.readData();
         System.out.println("Hello and welcome to theUFO report user interface.");
         System.out.println("What would you like to do?");
         System.out.println("1) Sort the data\n2) Find the most common UFO shape\n3) Find the state with the most sightings\n4) Find the total sightings per country\n5) Find the average length of the comment");

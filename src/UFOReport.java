776b28605f8a76487ee110ab8d682c02b2bfe540
Theo Kozak <theokozak@gmail.com>
2026-05-11 09:51:06 -0600

    initial commit

---
+++ src/UFOReport.java
@@ -0,0 +1,10 @@
+public class UFOReport {
+    String dateTime;
+    String country;
+    String city;
+    String state;
+    String shape;
+    String summary;
+    double lat;
+    double lng;
+}
